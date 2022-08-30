f621a5ccd1f5597effaba0ee543cd41b74dfaac8
Pablo Clavijo <76256398+pablodclavijo@users.noreply.github.com>
2022-08-30 14:35:03 -0300

    Update movie.js

--- api/src/controllers/movie.js
+++ api/src/controllers/movie.js
@@ -5,8 +5,8 @@ async function getMoviesHandler (req, res){
     const {name, genreId, order} = req.params
     if((order && genreId)) return res.status(400).send("name and genre can only be queryed one at a time")
     const movies = {}
-    if(name) movies = await moviesDbInfo.searchMoviesByTitle(name, order ? order : null).catch(err => {return status(500).send(err)})
-    if(genreId) movies = await moviesDbInfo.searchMoviesByGenre(genreId, order ? order : null).catch(err => {return status(500).send(err)})
+    if(name) movies = await moviesDbInfo.searchMoviesByTitle(name, order ? order : null).catch(err => {return res.status(500).send(err)})
+    if(genreId) movies = await moviesDbInfo.searchMoviesByGenre(genreId, order ? order : null).catch(err => {return res.status(500).send(err)})
     movies = await moviesDbInfo.getAllMovies().catch(err => {return status(500).send(err)})
     if(!movies) return res.status(404).send("movies not found")
     return res.status(200).send(movies)
@@ -16,7 +16,7 @@ async function getMoviesHandler (req, res){
 async function getMovieByIdHandler (req, res){
 
     const {id} = req.params
-    const movieDetail = await moviesDbInfo.getMovieDetail(id).catch(err => {return status(500).send(err)})
+    const movieDetail = await moviesDbInfo.getMovieDetail(id).catch(err => {return res.status(500).send(err)})
     if(!movieDetail) return res.status(404).send("movie's id doesn't match database")
     return res.status(200).send(movieDetail)
 }
@@ -24,7 +24,7 @@ async function getMovieByIdHandler (req, res){
 async function deleteMovieHandler (req, res) {
      
     const {id} = req.body
-    const isDeleted = await moviesDbInfo.deleteMovie(id).catch(err => {return status(500).send(err)})
+    const isDeleted = await moviesDbInfo.deleteMovie(id).catch(err => {return res.status(500).send(err)})
     if(!isDeleted) return res.status(400).send("failed")
     return res.status(200).send("movie succesfully deleted")
 }
@@ -32,7 +32,7 @@ async function deleteMovieHandler (req, res) {
 async function putMovieHandler (req, res) {
 
     const {id, newValues} = req.body
-    const isUpdated = await moviesDbInfo.updateMovie(id, newValues).catch(err => {return status(500).send(err)})
+    const isUpdated = await moviesDbInfo.updateMovie(id, newValues).catch(err => {return res.status(500).send(err)})
     if(!isUpdated) return res.status(400).send("update failed")
     return res.status(200).send(isUpdated)
 }
@@ -40,7 +40,7 @@ async function putMovieHandler (req, res) {
 async function postMovieHandler (req, res){
 
     const {movieData} = req.body
-    const isPosted = await moviesDbInfo.createMovie(movieData).catch(err => {return status(500).send(err)})
+    const isPosted = await moviesDbInfo.createMovie(movieData).catch(err => {return res.status(500).send(err)})
     if(!isPosted) return res.status(400).send("creatiion failed")
     return res.status(200).send(isPosted)
 }
