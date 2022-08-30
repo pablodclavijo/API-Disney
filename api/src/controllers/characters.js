d5f31a66a320e68daf1f85c798e9f8b0904c7e79
Pablo Clavijo <76256398+pablodclavijo@users.noreply.github.com>
2022-08-29 22:02:39 -0300

    Update characters.js

--- api/src/controllers/characters.js
+++ api/src/controllers/characters.js
@@ -11,7 +11,7 @@ async function getCharactersHandler (req, res){
     if(age) characters = await charactersDbInfo.searchCharacters("age", name)        // bc the function returns
     if(weight) characters = await charactersDbInfo.searchCharacters("weight", name)   //false when nothing found
     
-    characters = await charactersDbInfo.getAllCharacters().catch(err => console.log(err))
+    characters = await charactersDbInfo.getAllCharacters().catch(err => return status(500).send(err))
     if(!characters) return res.status(404).send("characters not found")
     return res.status(200).send(characters)
 
@@ -20,7 +20,7 @@ async function getCharactersHandler (req, res){
 async function getCharactersByIdHandler (req, res){
 
     const {id} = req.params
-    const characterDetail = await charactersDbInfo.getCharacterDetail(id).catch(err => consolelog(err))
+    const characterDetail = await charactersDbInfo.getCharacterDetail(id).catch(err => return status(500).send(err))
     if(!characterDetail) return res.status(404).send("characters id doesn't match database")
     return res.status(200).send(characterDetail)
 }
@@ -28,7 +28,7 @@ async function getCharactersByIdHandler (req, res){
 async function deleteCharacterHandler (req, res) {
      
     const {id} = req.body
-    const isDeleted = await charactersDbInfo.deleteCharacter(id).catch(err => console.log(err))
+    const isDeleted = await charactersDbInfo.deleteCharacter(id).catch(err => return status(500).send(err))
     if(!isDeleted) return res.status(400).send("failed")
     return res.status(200).send("character succesfully deleted")
 }
@@ -36,7 +36,7 @@ async function deleteCharacterHandler (req, res) {
 async function putCharacterHandler (req, res) {
 
     const {id, newValues} = req.body
-    const isUpdated = await charactersDbInfo.updateCharacter(id, newValues)
+    const isUpdated = await charactersDbInfo.updateCharacter(id, newValues).catch(err => return status(500).send(err))
     if(!isUpdated) return res.status(400).send("update failed")
     return res.status(200).send(isUpdated)
 }
@@ -44,7 +44,7 @@ async function putCharacterHandler (req, res) {
 async function postCharacterHandler (req, res){
 
     const {characterData} = req.body
-    const isPosted = await charactersDbInfo.createCharacter(characterData)
+    const isPosted = await charactersDbInfo.createCharacter(characterData).catch(err => return status(500).send(err))
     if(!isPosted) return res.status(400).send("creatiion failed")
     return res.status(200).send(isPosted)
 }
@@ -57,10 +57,3 @@ module.exports ={
 
 }
 
-/*  
-name    age     weight      fout    dout
-
-T       f          f          t        t
-T       t          f          f        
-
-*/
