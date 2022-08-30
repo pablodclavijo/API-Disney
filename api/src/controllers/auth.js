2e7701a9d28cef3d35ff542d6a907008613573e3
Pablo Clavijo <76256398+pablodclavijo@users.noreply.github.com>
2022-08-29 22:01:18 -0300

    Update auth.js

--- api/src/controllers/auth.js
+++ api/src/controllers/auth.js
@@ -5,7 +5,7 @@ async function postAuthRegisterController(req, res){
     const {email, password} = req.body
     const userExists = await getUser(email)
     if(userExists) return res.status(403).send("email already registered")
-    const user = await userCreate(email, password).catch(err => console.log(err))
+    const user = await userCreate(email, password).catch(err => return status(500).send(err))
     if(!user) return res.status(500).send("unable to register user")
     return res.status(200).send(user)
 
